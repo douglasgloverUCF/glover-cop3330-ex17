905462002a6ddc0c111c1270523bf944b4b81ed6
Douglas Glover <douglasglover3@knights.ucf.edu>
2021-06-06 18:08:56 -0400

    First Iteration

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,9 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Douglas Glover
+ */
 package base;
+import java.util.Scanner;
 /*
 Sometimes you have to perform a more complex calculation based on some provided inputs and then use that result to make a determination.
 
@@ -46,5 +51,44 @@ Look up the legal BAC limit by state and prompt for the state. Display a message
 Develop this as a mobile application that makes it easy to record each drink, updating the BAC each time a drink is entered.
  */
 public class App {
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        App myApp = new App();
 
+        System.out.print("Enter a 1 is you are male or a 2 if you are female: ");
+        int sex = myApp.getInt();
+
+        System.out.print("How many ounces of alcohol did you have? ");
+        int oz = myApp.getInt();
+
+        System.out.print("What is your weight, in pounds? ");
+        int weight = myApp.getInt();
+
+        System.out.print("How many hours has it been since your last drink? ");
+        int hours = myApp.getInt();
+
+        myApp.findBAC(sex, oz, weight, hours);
+    }
+    int getInt()
+    {
+        while(true) {
+            if (!in.hasNextInt())
+            {
+                in.next();
+                continue;
+            }
+            return in.nextInt();
+        }
+    }
+    void findBAC(int sex, int oz, int weight, int hours)
+    {
+        double ratio = sex == 1 ? 0.73 : 0.66;
+        double BAC = (oz * 5.14 / weight * ratio) - .015 * hours;
+        String legality = BAC > 0.08 ? "not legal" : "legal";
+
+        System.out.printf("""
+        Your BAC is %.6f
+        It is %s for you to drive.
+        """, BAC, legality);
+    }
 }
